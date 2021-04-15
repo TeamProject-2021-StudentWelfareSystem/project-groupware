4998c340b8d52b007865a0086322c883f29c43a0
Jungmin <happy6021005@daum.net>
2021-04-15 19:10:29 +0900

    mysql 단과대 전공 student, professor테이블로 이동

--- GroupWare.sql
+++ GroupWare.sql
@@ -3,28 +3,94 @@ show databases;
 create database GroupWare;
 use GroupWare;
 show tables;
-# 회원가입sql문
-insert into student(studentName, studentRR, studentNum, studentCampus, studentColleges, studentMajor, studentDoubleMajor, studentAddress, 
-studentPhoneNum, studentEmail, studentLoginID, studentLoginPwd, studentGrade) values (?,?,?,?,?,?,?,?,?,?,?,?,?);
-insert into professor(professorName, professorRR, professorCampus, professorColleges, professorMajor, professorAddress, 
-professorPhoneNum, professorEmail, professorLoginID, professorLoginPwd) values (?,?,?,?,?,?,?,?,?,?);
-insert into manager(managerName, managerRR, managerPhoneNum, managerEmail, managerLoginID, managerLoginPwd, managerDepartment) 
-values (?,?,?,?,?,?,?);
+
 # 로그인 날짜 입력
 update User set LoginDate = date_format(NOW(), '%Y%m%d') where UserName = "박지수";
+
 # insert 모음
 insert into UserEmail(UserEmail, UserCertificationNum, UserCertificationTime) values ("123@mju.ac.kr", "123456", date_format(NOW(), '%Y%m%d%H%m%s'));
-insert into User(UserName, UserPhoneNum, UserEmail, UserLoginID, UserLoginPwd) values ("박지수","010-3501-8711","happy6021005@naver.com","132","1234");
+insert into User(UserName, UserPhoneNum, UserEmail, UserLoginID, UserLoginPwd) values ("정민","01045018711","happy6021004@mju.ac.kr","60181666","wjdals0426@");
+insert into Student(StudentGender, StudentGrade, StudentColleges, StudentMajor, StudentDoubleMajor, UserID) values ("여자", "4학년", "ICT융합대학", "융합소프트웨어학부", "없음", 1);
+
 # select 모음
 select userLoginID, userName from user where userloginID = "학번" and userName = "이름";
 select * from User;
 select * from Student;
 select * from UserEmail;
+select StudentGrade,StudentGender,StudentDoubleMajor from Student where StudentID = '1';
+
 # drop 모음
 drop table Professor;
 drop table Student;
 drop table User;
 drop table UserEmail;
+
+# update 모음
+update User set UserLoginPwd = '바꿀 비밀번호' where UserLoginID = 'UserLoginID';
+update Student set StudentGender = '여' where UserID = '해당UserID';
+update User set UserPhoneNum = '바꿀 번호' where UserLoginID = 'UserLoginID';
+update Student set StudentGrade = '바꿀 학년' where UserLoginID = 'UserLoginID';
+update User set UserColleges = '바꿀 단과대학' where UserLoginID = 'UserLoginID';
+update User set UserMajor = '바꿀 학과' where UserLoginID = 'UserLoginID';
+update Student set StudentDoubleMajor = '바꿀 복수전공' where UserLoginID = 'UserLoginID'; 
+
+create table User(
+UserID int auto_increment not null primary key,
+UserName varchar(20) not null,
+UserPhoneNum varchar(30) not null,
+UserEmail varchar(100) not null unique key,
+UserLoginID varchar(30) binary not null unique key,
+UserLoginPwd varchar(300) binary not null,
+UserRole ENUM ('STUDENT', 'PROFESSOR', 'ADMINISTRATOR'),
+Authority varchar(20) not null default 'ROLE_USER', # ROLE_USER, ROLE_ADMIN
+Enabled boolean not null default 1, # 활성화:1 비활성화:0
+LoginDate date, #로그인날짜
+Withdrawal boolean not null default 0 # 가입:0 탈퇴:1 
+);
+
+# 회원가입 전 인증메일
+create table UserEmail(
+UserEmailID int auto_increment not null primary key,
+UserEmail varchar(100) not null, 
+UserCertificationNum int, #인증번호
+UserCertificationTime Datetime #인증번호 받은 시간
+);
+
+create table Student(
+StudentID int auto_increment not null primary key,
+StudentGrade ENUM ('1학년', '2학년', '3학년', '4학년') not null, #학년
+StudentGender varchar(20) not null, # male / female
+StudentColleges ENUM ('인문대학', '사회과학대학', '경영대학', '법과대학', 'ICT융합대학', '미래융합대학') not null, #단과대학
+StudentMajor ENUM ('국어국문학과', '영어영문학과', '중어중문학과', '일어일문학과', '사학과', '문헌정보학과', '아랍지역학과', '미술사학과', '철학과', '문예창작학과', 
+'행정학과', '경제학과', '정치외교학과', '디지털미디어학과', '아동학과', '청소년지도학과',
+'경영정보학과', '국제통상학과',
+'법학과',
+'융합소프트웨어학부', '디지털콘텐츠디자인학과',
+'창의융합인재학부', '사회복지학과', '부동산학과', '법무행정학과', '심리치료학과', '미래융합경영학과', '멀티디자인학과', '계약학과') not null, #전공
+StudentDoubleMajor ENUM ('국어국문학과', '영어영문학과', '중어중문학과', '일어일문학과', '사학과', '문헌정보학과', '아랍지역학과', '미술사학과', '철학과', '문예창작학과', 
+'행정학과', '경제학과', '정치외교학과', '디지털미디어학과', '아동학과', '청소년지도학과',
+'경영정보학과', '국제통상학과',
+'법학과',
+'융합소프트웨어학부', '디지털콘텐츠디자인학과',
+'창의융합인재학부','사회복지학과', '부동산학과', '법무행정학과', '심리치료학과', '미래융합경영학과', '멀티디자인학과', '계약학과', '없음') default '없음', #복수전공
+UserID int, foreign key (StudentID) references user(UserID) on delete cascade on update cascade
+);
+
+create table Professor(
+ProfessorID int auto_increment not null primary key,
+ProfessorRoom varchar(10), #교수실
+ProfessorRoomNum varchar(30), #교수실전화번호 
+ProfessorColleges ENUM ('인문대학', '사회과학대학', '경영대학', '법과대학', 'ICT융합대학', '미래융합대학') not null, #단과대학
+ProfessorMajor ENUM ('국어국문학과', '영어영문학과', '중어중문학과', '일어일문학과', '사학과', '문헌정보학과', '아랍지역학과', '미술사학과', '철학과', '문예창작학과', 
+'행정학과', '경제학과', '정치외교학과', '디지털미디어학과', '아동학과', '청소년지도학과',
+'경영정보학과', '국제통상학과',
+'법학과',
+'융합소프트웨어학부', '디지털콘텐츠디자인학과',
+'창의융합인재학부', '사회복지학과', '부동산학과', '법무행정학과', '심리치료학과', '미래융합경영학과', '멀티디자인학과', '계약학과') not null, #전공
+UserID int, foreign key (ProfessorID) references user(UserID) on delete cascade on update cascade
+);
+
+
 # 하루 한 번 인증번호 삭제
 CREATE
    EVENT email_validation_Scheduler ON SCHEDULE EVERY 1 DAY STARTS '2021-04-09 00:00:00'
@@ -48,60 +114,12 @@ CREATE
    Event withdrawal_Scheduler_test ON SCHEDULE EVERY 1 day STARTS '2021-04-09'
     DO
     UPDATE User set Withdrawal = 1 WHERE LoginDate <= DATE_SUB(NOW(), INTERVAL 6 month);
+
 DROP EVENT email_validation_Scheduler;
 DROP EVENT email_validation_Scheduler_test;
 DROP EVENT withdrawal_Scheduler;
 DROP EVENT withdrawal_Scheduler_test;
 
-select * from user;
-UPDATE User
-set Authority = 'ROLE_ADMIN'
-where UserName = '박지수';
-create table User(
-UserID int auto_increment not null primary key,
-UserName varchar(20) not null,
-UserPhoneNum varchar(30) not null,
-UserEmail varchar(100) not null unique key,
-UserLoginID varchar(30) binary not null unique key,
-UserLoginPwd varchar(300) binary not null,
-UserRole ENUM ('STUDENT', 'PROFESSOR', 'ADMINISTRATOR'),
-Authority varchar(20) not null default 'ROLE_USER', # ROLE_USER, ROLE_ADMIN
-Enabled boolean not null default 1, # 활성화:1 비활성화:0
-LoginDate date, #로그인날짜
-UserColleges ENUM ('인문대학', '사회과학대학', '경영대학', '법과대학', 'ICT융합대학', '미래융합대학') not null, #단과대학
-UserMajor ENUM ('국어국문학과', '영어영문학과', '중어중문학과', '일어일문학과', '사학과', '문헌정보학과', '아랍지역학과', '미술사학과', '철학과', '문예창작학과', 
-'행정학과', '경제학과', '정치외교학과', '디지털미디어학과', '아동학과', '청소년지도학과',
-'경영정보학과', '국제통상학과',
-'법학과',
-'융합소프트웨어학부', '디지털콘텐츠디자인학과',
-'창의융합인재학부', '사회복지학과', '부동산학과', '법무행정학과', '심리치료학과', '미래융합경영학과', '멀티디자인학과', '계약학과') not null, #전공
-Withdrawal boolean not null default 0 # 가입:0 탈퇴:1 
-);
-# 회원가입 전 인증메일
-create table UserEmail(
-UserEmailID int auto_increment not null primary key,
-UserEmail varchar(100) not null, 
-UserCertificationNum int, #인증번호
-UserCertificationTime Datetime #인증번호 받은 시간
-);
-create table Student(
-StudentID int auto_increment not null primary key,
-StudentGrade ENUM ('1학년', '2학년', '3학년', '4학년') not null, #학년
-StudentGender varchar(20) not null, # male / female
-StudentDoubleMajor ENUM ('국어국문학과', '영어영문학과', '중어중문학과', '일어일문학과', '사학과', '문헌정보학과', '아랍지역학과', '미술사학과', '철학과', '문예창작학과', 
-'행정학과', '경제학과', '정치외교학과', '디지털미디어학과', '아동학과', '청소년지도학과',
-'경영정보학과', '국제통상학과',
-'법학과',
-'융합소프트웨어학부', '디지털콘텐츠디자인학과',
-'창의융합인재학부','사회복지학과', '부동산학과', '법무행정학과', '심리치료학과', '미래융합경영학과', '멀티디자인학과', '계약학과','없음') Default '없음', #복수전공
-UserID int, foreign key (UserID) references user(UserID) on delete cascade on update cascade
-);
-create table Professor(
-ProfessorID int auto_increment not null primary key,
-ProfessorRoom varchar(10), #교수실
-ProfessorRoomNum varchar(30), #교수실전화번호 
-UserID int, foreign key (UserID) references user(UserID) on delete cascade on update cascade
-);
 /*
 * ON DELETE SET NULL
 * ON UPDATE SET NULL
