afa997e9f92886db00c07809e4606e830d3ce0fe
Jungmin <happy6021005@daum.net>
2021-04-19 17:41:50 +0900

    탈퇴 기능 개선
    JSP와 JS가 살짝 수정되었습니다
    1. withdrawal.js -> 탈퇴 버튼 후 return false를 return withdrawal 로 변경
    2. withdrawal.jsp -> 탈퇴 확인 버튼 type = button에서 type = submit으로 변경되었습니다 확인부탁드립니다.

---
+++ GroupwareD10/src/main/java/com/mju/groupware/controller/AdministratorController.java
@@ -0,0 +1,88 @@
+package com.mju.groupware.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mju.groupware.dto.UserList;
+import com.mju.groupware.service.AdminService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdministratorController {
+
+	@Autowired
+	private AdminService adminService;
+
+	/* 관리자 메뉴 메인화면 */
+	@RequestMapping(value = "/manageList", method = RequestMethod.GET)
+	public String manageList(Model model) {
+
+		try {
+			List<UserList> list = adminService.list();
+
+			model.addAttribute("list", list);
+			// 여기서 선택된 학생번호 받아오시면됩니다.
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "/admin/manageList";
+
+	}
+
+	/* 관리자 메뉴-휴면 계정 관리 화면 */
+	@RequestMapping(value = "/manageSleep", method = RequestMethod.GET)
+	public String manageSleep(Model model) {
+
+		try {
+			List<UserList> DormantList = adminService.DormantList();
+
+			model.addAttribute("DormantList", DormantList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "/admin/manageSleep";
+	}
+
+	/* 관리자 메뉴-탈퇴 계정 관리 화면 */
+	@RequestMapping(value="/manageSecession", method = RequestMethod.GET)
+	public String manageSecession(Model model) {
+		try {
+			List<UserList> WithdrawalList = adminService.WithdrawalList();
+
+			model.addAttribute("WithdrawalList", WithdrawalList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "/admin/manageSecession";
+	}
+
+	/* 관리자 메뉴-회원 목록 클릭 시 정보 출력 화면 */
+	@RequestMapping(value = "/manageStudent", method = RequestMethod.GET)
+	public String manageStudent() {
+		return "manageStudent";
+	}
+
+	@RequestMapping(value = "/manageProfessor", method = RequestMethod.GET)
+	public String manageProfessor() {
+		return "manageProfessor";
+	}
+
+	@RequestMapping(value = "/manageModifyStudent", method = RequestMethod.GET)
+	public String manageModifyStudent() {
+		return "manageModifyStudent";
+
+	}
+
+	@RequestMapping(value = "/manageModifyProfessor", method = RequestMethod.GET)
+	public String manageModifyProfessor() {
+		return "manageModifyProfessor";
+
+	}
+
+}
