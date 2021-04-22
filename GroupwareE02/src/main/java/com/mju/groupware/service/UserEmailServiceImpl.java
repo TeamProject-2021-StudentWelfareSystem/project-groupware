f9d0554cffc0321b71d279f2a79d1185c852bcd5
Jungmin <happy6021005@daum.net>
2021-04-22 17:12:45 +0900

    UserFunction 리펙토링

---
+++ GroupwareE02/src/main/java/com/mju/groupware/service/UserEmailServiceImpl.java
@@ -0,0 +1,31 @@
+package com.mju.groupware.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mju.groupware.dao.UserEmailDao;
+import com.mju.groupware.dto.UserEmail;
+
+@Service
+public class UserEmailServiceImpl implements UserEmailService {
+
+	@Autowired
+	UserEmailDao userEmailDao;
+	
+	@Override
+	public void InsertCertification(UserEmail userEmail) {
+		userEmailDao.InsertCertification(userEmail);
+	}
+
+	@Override
+	public boolean CheckCertification(String authNum) {
+		boolean checker = userEmailDao.CheckCertificationNum(Integer.parseInt(authNum));
+		return checker;
+	}
+
+	@Override
+	public void DeleteInfo(UserEmail userEmail) {
+		userEmailDao.DeleteCertification(userEmail);
+	}
+
+}
