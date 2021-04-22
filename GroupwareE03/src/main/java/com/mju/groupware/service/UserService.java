e82214c38cfcb26d8183053680788cbd528502da
Jungmin <happy6021005@daum.net>
2021-04-22 22:08:08 +0900

    서버팀 리펙토링(메소드, 변수명 변경) 완료

---
+++ GroupwareE03/src/main/java/com/mju/groupware/service/UserService.java
@@ -0,0 +1,46 @@
+package com.mju.groupware.service;
+
+import java.util.ArrayList;
+
+import com.mju.groupware.dto.Student;
+import com.mju.groupware.dto.User;
+
+public interface UserService {
+
+	// 회원가입
+	public void InsertForSignUp(User user);
+	// 중복확인
+	public boolean SelctForIDConfirm(User user);
+	// 비번 찾기
+	public boolean SelectPwdForConfirmToFindPwd(User user);
+	// userID(다른 테이블들의 foreign key) 찾기
+	public int SelectUserID(Student student);
+
+	public String SelectForShowPassword(User user);
+
+	public void UpdateLoginDate(User user);
+
+	public String SelectCurrentPwd(String id);
+
+	public void UpdatePwd(User user);
+
+	public boolean SelectForPwdCheckBeforeModify(String pw, String pw2);
+
+	public ArrayList<String> SelectMyPageUserInfo(String userId);
+
+	public ArrayList<String> SelectUserProfileInfo(String loginID);
+
+	public void updateUserPhoneNumber(User user);
+
+	public void UpdateUserMajor(User user);
+	
+	public void UpdateUserColleges(User user);
+
+	public ArrayList<String> SelectUserInformation(String userId);
+
+	public void UpdateTemporaryPwd(User user);
+
+	public void UpdateWithdrawlUser(String id);
+
+
+}
