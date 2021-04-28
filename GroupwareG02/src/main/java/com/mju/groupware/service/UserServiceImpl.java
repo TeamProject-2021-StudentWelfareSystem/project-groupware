bd704d03f7bacca8b3c8986ffe4529c7c43b3036
lsb02 <subin2951@gmail.com>
2021-04-29 01:19:12 +0900

    Detail 화면 구현 완료 / 메일 로그인, 리스트, 내용 출력 화면 구현
    1. Admin 컨트롤러 추가
    - manageList에서 회원 아이디, 이름 클릭 시 회원 role에 따라 페이지 리턴
    - manageList에서 회원 아이디, 이름 클릭 시 학생/교수 정보 출력
    - manageList.jsp 변경
    2. manageModify.js 파일 추가
    3. manageModifyStudent, Professor.jsp 변경 (단과대학 선택시 전공 선택 가능 복수전공 show/hide 가능)
    4. signupProfessor.jsp, js 파일 변경
    
    5. 회원가입 시 인증번호 받는 email.jsp 파일 이름 emailAuthentication으로 변경
    - css 이름 변경
    - user function controller 변경
    - infoConsent.jsp 경로까지 변경 완료!
    
    6. 이메일 로그인 화면(emailLogin.jsp), emailList.jsp/css, emailContent.jsp/css 구현

---
+++ GroupwareG02/src/main/java/com/mju/groupware/service/UserServiceImpl.java
@@ -0,0 +1,190 @@
+package com.mju.groupware.service;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mju.groupware.dao.StudentDao;
+import com.mju.groupware.dao.UserDao;
+import com.mju.groupware.dto.Student;
+import com.mju.groupware.dto.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private StudentDao studentDao;
+
+	@Override
+	public void InsertForSignUp(User user) {
+		userDao.InsertForSignUp(user);
+	}
+
+	@Override
+	public boolean SelctForIDConfirm(User user) {
+		boolean Checker = userDao.SelctForIDConfirm(user);
+		return Checker;
+	}
+
+	@Override
+	public boolean SelectPwdForConfirmToFindPwd(User user) {
+		boolean PwdChecker = userDao.SelectPwdForConfirmToFindPwd(user);
+		return PwdChecker;
+	}
+
+	@Override
+	public String SelectForShowPassword(User user) {// 임시 비밀번호 생성
+		boolean Checker = userDao.SelectForShowPassword(user);
+		Random Random = new Random();
+		String Result = "";
+		if (Checker) {
+			Result = Integer.toString(Random.nextInt(8) + 1);
+			for (int i = 0; i < 7; i++) {
+				Result += Integer.toString(Random.nextInt(9));
+			}
+		} else {
+			Result = "false";
+		}
+		return Result;
+	}
+
+	@Override
+	public int SelectUserID(Student student) {
+		return userDao.SelectUserID(student);
+	}
+
+	@Override
+	public void UpdateLoginDate(User user) {
+		userDao.UpdateLoginDate(user);
+	}
+
+	@Override
+	public String SelectCurrentPwd(String id) {
+		return userDao.SelectCurrentPwd(id);
+	}
+
+	@Override
+	public void UpdatePwd(User user) {
+		userDao.UpdatePwd(user);
+	}
+
+	@Override
+	public ArrayList<String> SelectUserProfileInfo(String id) {
+		ArrayList<String> Info = new ArrayList<String>();
+		Info = userDao.SelectUserProfileInfo(id);
+		return Info;
+	}
+
+	@Override
+	public void updateUserPhoneNumber(User user) {
+		userDao.updateUserPhoneNumber(user);
+	}
+
+	@Override
+	public void UpdateUserMajor(User user) {
+		userDao.updateUserMajor(user);
+	}
+
+	@Override
+	public void UpdateUserColleges(User user) {
+		userDao.updateUserColleges(user);
+	}
+
+	@Override
+	public ArrayList<String> SelectUserInformation(String userId) {
+		ArrayList<String> UserInfo = new ArrayList<String>();
+		UserInfo = userDao.SelectUserInformation(userId);
+		return UserInfo;
+	}
+
+	@Override
+	public boolean SelectForPwdCheckBeforeModify(String id, String pw) {
+		return userDao.SelectForPwdCheckBeforeModify(id, pw);
+	}
+
+	@Override
+	public void UpdateTemporaryPwd(User user) {
+		userDao.UpdateTemporaryPwd(user);
+
+	}
+
+	@Override
+	public void UpdateWithdrawlUser(String id) {
+		userDao.UpdateWithdrawlUser(id);
+	}
+
+	@Override
+	public void UpdateDoWithdrawalRecoveryByAdmin(String id) {
+		userDao.UpdateDoWithdrawalRecoveryByAdmin(id);
+	}
+
+	@Override
+	public void UpdateDormantOneToZero(String id) {
+		userDao.UpdateDormantOneToZero(id);
+	}
+
+	@Override
+	public void UpdateUserRole(String id, String optionValue) {
+
+		userDao.UpdateUserRole(id, optionValue);
+
+	}
+
+	@Override
+	public void UpdateAdminRole(String id, String optionValue) {
+		userDao.UpdateAdminRole(id, optionValue);
+
+	}
+
+	@Override
+	public ArrayList<String> SelectMyPageUserInfo(String userId) {
+		ArrayList<String> Info = new ArrayList<String>();
+		ArrayList<String> UserInfo = new ArrayList<String>();
+		ArrayList<String> StudentInfo = new ArrayList<String>();
+
+		UserInfo = userDao.SelectMyPageUserInfo(userId);
+		StudentInfo = studentDao.SelectMyPageUserInfo(UserInfo.get(0));
+		UserInfo.remove(0);
+
+		for (int i = 0; i < UserInfo.size(); i++) {
+			Info.add(UserInfo.get(i));
+		}
+		for (int i = 0; i < StudentInfo.size(); i++) {
+			Info.add(StudentInfo.get(i));
+		}
+		return Info;
+	}
+
+	@Override
+	public ArrayList<String> SelectUserProfileInfoByID(String mysqlID) {
+		ArrayList<String> Info = new ArrayList<String>();
+		ArrayList<String> UserInfo = new ArrayList<String>();
+		ArrayList<String> StudentInfo = new ArrayList<String>();
+		// 유저정보를 mysqlID를 던져줘서 받아온다.
+		UserInfo = userDao.SelectMyPageUserInfoByID(mysqlID);
+		// 학생정보를 mysqlID를 통해서 받아온다.
+		StudentInfo = studentDao.SelectMyPageUserInfoByID(mysqlID);
+		//Data의 크기만큼 Info List에 채워준다.
+		for (int i = 0; i < UserInfo.size(); i++) {
+			Info.add(UserInfo.get(i));
+		}
+		//Data의 크기만큼 Info List에 채워준다.
+		for (int i = 0; i < StudentInfo.size(); i++) {
+			Info.add(StudentInfo.get(i));
+		}
+		return Info;
+	}
+
+	@Override
+	public void UpdateUserName(User user) {
+		
+		userDao.UpdateUserName(user);
+		
+	}
+
+}
