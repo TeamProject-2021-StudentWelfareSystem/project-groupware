de55f38c26de79cf334fc53640e72b65201d6803
Honam Kim <48897023+tigerpoint123@users.noreply.github.com>
2021-05-01 17:48:22 +0900

    이메일 리스트 구현중

---
+++ GroupwareG05/src/main/java/com/mju/groupware/service/EmailService.java
@@ -0,0 +1,19 @@
+package com.mju.groupware.service;
+
+import java.util.List;
+
+import com.mju.groupware.dto.User;
+
+public interface EmailService {
+	// 이메일 보내기
+	public int sendEmail(User user);
+
+	// 인증번호확인
+	public boolean AuthNum(String authNum);
+
+	// 이메일중복확인
+	public boolean SelectForEmailDuplicateCheck(User user);
+
+	public List<String> printEmailList(String id, String pw);
+
+}
