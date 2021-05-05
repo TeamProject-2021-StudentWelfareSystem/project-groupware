9cec3c40a96001bcd04af76143a7a647ddb70593
Honam Kim <48897023+tigerpoint123@users.noreply.github.com>
2021-05-05 17:56:00 +0900

    properties xml 구현( 적용 x)

---
+++ GroupwareH05/src/main/java/com/mju/groupware/constant/constant.java
@@ -0,0 +1,27 @@
+package com.mju.groupware.constant;
+
+public class constant {
+	private String Host;
+	private String HostEmail;
+	private String HostPwd;
+	
+	public String getHost() {
+		return Host;
+	}
+	public void setHost(String host) {
+		Host = host;
+	}
+	public String getHostEmail() {
+		return HostEmail;
+	}
+	public void setHostEmail(String hostEmail) {
+		HostEmail = hostEmail;
+	}
+	public String getHostPwd() {
+		return HostPwd;
+	}
+	public void setHostPwd(String hostPwd) {
+		HostPwd = hostPwd;
+	}
+
+}
