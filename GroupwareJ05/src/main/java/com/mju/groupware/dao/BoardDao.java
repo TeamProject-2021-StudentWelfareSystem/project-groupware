4cd2f3dba194d714d6d61f8b0334a04cc15b9035
Honam Kim <48897023+tigerpoint123@users.noreply.github.com>
2021-05-12 22:58:38 +0900

    게시판 글 작성 + 파일 업로드
    파일 다운로드, 게시글 수정 등은 미완성

---
+++ GroupwareJ05/src/main/java/com/mju/groupware/dao/BoardDao.java
@@ -0,0 +1,22 @@
+package com.mju.groupware.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.mju.groupware.dto.Board;
+
+public interface BoardDao {
+
+	void InsertBoardInfo(Board board);
+
+	void InsertFile(Map<String, Object> map);
+
+	List<Board> SelectCommunityBoardList();
+
+	void UpdateHitCount(String boardID);
+
+	void insertFile(Map<String, Object> map);
+
+	int SelectBoardID(Board board);
+
+}
