03a397a6c7d71e07f54a6c8dc6ffa43b9731c9c8
Jungmin <happy6021005@daum.net>
2021-05-15 18:19:24 +0900

    게시글작성+파일업로드 오류 수정

---
+++ GroupwareJ11/src/main/java/com/mju/groupware/service/BoardService.java
@@ -0,0 +1,25 @@
+package com.mju.groupware.service;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.mju.groupware.dto.Board;
+
+public interface BoardService {
+
+//	void InsertBoardInfo(Board board, MultipartHttpServletRequest mpRequest);
+
+	void InsertBoard(Board board, HttpServletRequest request);
+
+	List<Board> SelectCommunityBoardList();
+
+	void UpdateHitCount(String boardID);
+
+	Board SelectOneCommunityContent(String boardID);
+
+	void UpdateModifiedContent(Board board);
+
+	String SelectLoginUserID(String loginID);
+
+}
