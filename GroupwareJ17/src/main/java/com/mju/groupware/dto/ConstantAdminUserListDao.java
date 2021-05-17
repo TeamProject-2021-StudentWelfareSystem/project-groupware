3b546e9b4c7e3ffe3cc06bb80897b91a2d90fde6
Honam Kim <48897023+tigerpoint123@users.noreply.github.com>
2021-05-17 18:52:38 +0900

    다운로드 고쳐줘 벅벅

---
+++ GroupwareJ17/src/main/java/com/mju/groupware/dto/ConstantAdminUserListDao.java
@@ -0,0 +1,62 @@
+package com.mju.groupware.dto;
+
+public class ConstantAdminUserListDao {
+	private String UserListDaoID;
+	private String SelectUserList;
+	private String SelectDormantList;
+	private String SelectWithdrawalList;
+	private String SelectWithdrawalUserListForRecovery;
+	private String SelectWithdrawalStudentListForRecovery;
+	private String InsertUserForRecovery;
+	private String InsertStudentForRecovery;
+	
+	public String getUserListDaoID() {
+		return UserListDaoID;
+	}
+	public void setUserListDaoID(String userListDaoID) {
+		UserListDaoID = userListDaoID;
+	}
+	public String getSelectUserList() {
+		return SelectUserList;
+	}
+	public void setSelectUserList(String selectUserList) {
+		SelectUserList = selectUserList;
+	}
+	public String getSelectDormantList() {
+		return SelectDormantList;
+	}
+	public void setSelectDormantList(String selectDormantList) {
+		SelectDormantList = selectDormantList;
+	}
+	public String getSelectWithdrawalList() {
+		return SelectWithdrawalList;
+	}
+	public void setSelectWithdrawalList(String selectWithdrawalList) {
+		SelectWithdrawalList = selectWithdrawalList;
+	}
+	public String getSelectWithdrawalUserListForRecovery() {
+		return SelectWithdrawalUserListForRecovery;
+	}
+	public void setSelectWithdrawalUserListForRecovery(String selectWithdrawalUserListForRecovery) {
+		SelectWithdrawalUserListForRecovery = selectWithdrawalUserListForRecovery;
+	}
+	public String getSelectWithdrawalStudentListForRecovery() {
+		return SelectWithdrawalStudentListForRecovery;
+	}
+	public void setSelectWithdrawalStudentListForRecovery(String selectWithdrawalStudentListForRecovery) {
+		SelectWithdrawalStudentListForRecovery = selectWithdrawalStudentListForRecovery;
+	}
+	public String getInsertUserForRecovery() {
+		return InsertUserForRecovery;
+	}
+	public void setInsertUserForRecovery(String insertUserForRecovery) {
+		InsertUserForRecovery = insertUserForRecovery;
+	}
+	public String getInsertStudentForRecovery() {
+		return InsertStudentForRecovery;
+	}
+	public void setInsertStudentForRecovery(String insertStudentForRecovery) {
+		InsertStudentForRecovery = insertStudentForRecovery;
+	}
+	
+}
