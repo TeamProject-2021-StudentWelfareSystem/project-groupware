d0068983bcd1fa41fb3b65d08245f1ee296bb204
Honam Kim <48897023+tigerpoint123@users.noreply.github.com>
2021-05-23 02:21:19 +0900

    팀 생성 진행중(PM만 받을 것)

---
+++ GroupwareK10/src/main/java/com/mju/groupware/dto/ConstantAdmin.java
@@ -0,0 +1,196 @@
+package com.mju.groupware.dto;
+
+public class ConstantAdmin {
+
+	private String PRole;
+	private String SRole;
+	private String ARole;
+	private String TemporaryPwd;
+
+	private String Home;
+	private String List;
+	private String ReList;
+	private String ReSleep;
+	private String SleepList;
+	private String SecessionList;
+	private String Detail;
+	private String ReSDetail;
+	private String RePDetail;
+	private String SDetail;
+	private String PDetail;
+	private String SModify;
+	private String PModify;
+	private String SManage;
+	private String PManage;
+	private String SManageModify;
+	private String PManageModify;
+
+	public String getPRole() {
+		return PRole;
+	}
+
+	public void setPRole(String pRole) {
+		PRole = pRole;
+	}
+
+	public String getSRole() {
+		return SRole;
+	}
+
+	public void setSRole(String sRole) {
+		SRole = sRole;
+	}
+
+	public String getARole() {
+		return ARole;
+	}
+
+	public void setARole(String aRole) {
+		ARole = aRole;
+	}
+
+	public String getTemporaryPwd() {
+		return TemporaryPwd;
+	}
+
+	public void setTemporaryPwd(String temporaryPwd) {
+		TemporaryPwd = temporaryPwd;
+	}
+
+	public String getHome() {
+		return Home;
+	}
+
+	public void setHome(String home) {
+		Home = home;
+	}
+
+	public String getList() {
+		return List;
+	}
+
+	public void setList(String list) {
+		List = list;
+	}
+
+	public String getReList() {
+		return ReList;
+	}
+
+	public void setReList(String reList) {
+		ReList = reList;
+	}
+
+	public String getReSleep() {
+		return ReSleep;
+	}
+
+	public void setReSleep(String reSleep) {
+		ReSleep = reSleep;
+	}
+
+	public String getSleepList() {
+		return SleepList;
+	}
+
+	public void setSleepList(String sleepList) {
+		SleepList = sleepList;
+	}
+
+	public String getSecessionList() {
+		return SecessionList;
+	}
+
+	public void setSecessionList(String secessionList) {
+		SecessionList = secessionList;
+	}
+
+	public String getDetail() {
+		return Detail;
+	}
+
+	public void setDetail(String detail) {
+		Detail = detail;
+	}
+
+	public String getReSDetail() {
+		return ReSDetail;
+	}
+
+	public void setReSDetail(String reSDetail) {
+		ReSDetail = reSDetail;
+	}
+
+	public String getRePDetail() {
+		return RePDetail;
+	}
+
+	public void setRePDetail(String rePDetail) {
+		RePDetail = rePDetail;
+	}
+
+	public String getSDetail() {
+		return SDetail;
+	}
+
+	public void setSDetail(String sDetail) {
+		SDetail = sDetail;
+	}
+
+	public String getPDetail() {
+		return PDetail;
+	}
+
+	public void setPDetail(String pDetail) {
+		PDetail = pDetail;
+	}
+
+	public String getSModify() {
+		return SModify;
+	}
+
+	public void setSModify(String sModify) {
+		SModify = sModify;
+	}
+	
+	public String getPModify() {
+		return PModify;
+	}
+
+	public void setPModify(String pModify) {
+		PModify = pModify;
+	}
+
+	public String getSManage() {
+		return SManage;
+	}
+
+	public void setSManage(String sManage) {
+		SManage = sManage;
+	}
+
+	public String getPManage() {
+		return PManage;
+	}
+
+	public void setPManage(String pManage) {
+		PManage = pManage;
+	}
+
+	public String getSManageModify() {
+		return SManageModify;
+	}
+
+	public void setSManageModify(String sManageModify) {
+		SManageModify = sManageModify;
+	}
+
+	public String getPManageModify() {
+		return PManageModify;
+	}
+
+	public void setPManageModify(String pManageModify) {
+		PManageModify = pManageModify;
+	}
+
+}
---
+++ GroupwareK10/src/main/java/com/mju/groupware/dto/ConstantWithdrawal.java
@@ -0,0 +1,41 @@
+package com.mju.groupware.dto;
+
+public class ConstantWithdrawal {
+
+	private String Parameter1;
+	private String Parameter2;
+	private String Parameter3;
+	private String Url;
+
+	public String getUrl() {
+		return Url;
+	}
+
+	public void setUrl(String url) {
+		Url = url;
+	}
+
+	public String getParameter3() {
+		return Parameter3;
+	}
+
+	public void setParameter3(String parameter3) {
+		Parameter3 = parameter3;
+	}
+
+	public String getParameter1() {
+		return Parameter1;
+	}
+
+	public String getParameter2() {
+		return Parameter2;
+	}
+
+	public void setParameter2(String parameter2) {
+		Parameter2 = parameter2;
+	}
+
+	public void setParameter1(String parameter1) {
+		Parameter1 = parameter1;
+	}
+}
