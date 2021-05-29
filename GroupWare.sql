5c4e28f37c9f81f7a31d4dd5a52235c9b9695b5c
Jungmin <happy6021005@daum.net>
2021-05-30 03:58:06 +0900

    Update GroupWare.sql

--- GroupWare.sql
+++ GroupWare.sql
@@ -8,6 +8,7 @@ show tables;
 delete from WithdrawalUser where WUserName = "탈퇴";
 delete from WithdrawalUser where WUserName = "자퇴";
 delete from User where UserID = "3";
+delete from Board where BoardSubject = "공지";
 
 # insert 모음
 insert into UserReservation(ReservationDate,ReservationStartTime,ReservationEndTime,ReservationNumOfPeople,LectureRoomNo,UserID) values ('2021-05-12','17:00:00','19:00:00',10,1135,14);
@@ -27,6 +28,8 @@ alter table Student drop column ProfessorRoom;
 alter table Team add TeamLeaderID varchar(30) not null;
 alter table TeamBoard add TBoardDelete boolean default 0 not null;
 alter table TeamBoard add TUserLoginID varchar(30) not null;
+alter table Board add BoardDelete boolean default 0 not null;
+alter table TeamFile add TFileDelete boolean default 0 not null;
 
 # select 모음
 select userLoginID, userName from user where userloginID = "학번" and userName = "이름";
@@ -59,7 +62,7 @@ select * from UserReservation where ReservationDate = '2021-05-12' and
 select ReservationNo, RoomLocation, ReservationDate, ReservationStartTime, ReservationStartTime
 from LectureRoom join UserReservation on LectureRoom.LectureRoomNo = UserReservation.ReservationNo;
 select * from LectureRoom join UserReservation on LectureRoom.LectureRoomNo = UserReservation.ReservationNo;
-
+SELECT BOriginalFileName,BStoredFileName from BoardFile where BFileID = 1 and BDelete = 0 order by BFileID ASC;
 # 유저가 예약하고자 하는 강의실을 선택 -> 해당 강의실을 선택하면 날짜 선택 -> 날짜 선택하면 해당 강의실No의 예약된 시간들을 select해옴
 # -> select해서 startTime, endTime의 시간들을 비교해서 없으면 예약 가능
 
@@ -132,6 +135,7 @@ IBoardWriter varchar(20) not null,
 IBoardDate dateTime not null,
 IBoardType varchar(50) not null,
 IBoardDelete boolean default 0,
+State varchar(20) not null,
 UserEmail varchar(100) not null,
 UserPhoneNum varchar(30) not null,
 UserID int not null,
@@ -160,6 +164,7 @@ BFileID int auto_increment not null primary key,
 BOriginalFileName varchar(200) not null,
 BStoredFileName varchar(200) not null,
 BFileSize int not null,
+BDelete boolean default 0 not null,
 BoardID int not null,
 foreign key (BoardID) references Board(BoardID) on delete cascade on update cascade
 );
@@ -181,17 +186,14 @@ ClassID int not null,
 foreign key (ClassID) references Class(ClassID) on delete cascade on update cascade
 );
 
-select date_format(ScheduleEndDate,'%Y-%m-%d %H:%i') as date from UserSchedule;
-select * from UserSchedule;
-drop table UserSchedule;
 create table UserSchedule(
 ScheduleID int auto_increment not null primary key,
-title varchar(50) not null,
-description varchar(100) not null,
-start varchar(100) not null,
-end varchar(100) not null,
-backgroundColor varchar(30) not null,
-allDay boolean,
+ScheduleTitle varchar(50) not null,
+ScheduleDesciption varchar(100) not null,
+ScheduleStartDate datetime not null,
+ScheduleEndDate datetime not null,
+BackgroundColor varchar(30) not null,
+UserName varchar(30) not null,
 UserID int not null,
 foreign key (UserID) references User(UserID) on delete cascade on update cascade
 );
@@ -212,6 +214,7 @@ TBoardContent varchar(10000) not null,
 TBoardWriter varchar(20) not null,
 TBoardDate dateTime not null,
 TBoardDelete boolean default 0 not null,
+TUserLoginID varchar(30) not null,
 TeamID int not null,
 foreign key (TeamID) references Team(TeamID) on delete cascade on update cascade
 );
@@ -222,9 +225,11 @@ TOriginalFileName varchar(200) not null,
 TStoredFileName varchar(200) not null,
 TFileSize int not null,
 TBoardID int not null,
+TFileDelete boolean default 0 not null,
 foreign key (TBoardID) references TeamBoard(TBoardID) on delete cascade on update cascade
 );
 
+select * from TeamFile;
 create table LectureRoom(
 LectureRoomNo int not null primary key,
 RoomLocation varchar(20) not null,
