5b7096c0c84acd5189496e0a8ea5b5f278f4e14a
YunjeongChoi <48897085+yjyjChoi@users.noreply.github.com>
2021-05-29 23:22:55 +0900

    일정 받아오기 가능 / 화면에 보여주는 건X
    create table UserSchedule(
    ScheduleID int auto_increment not null primary key,
    ScheduleTitle varchar(50) not null,
    ScheduleDesciption varchar(100) not null,
    ScheduleStartDate datetime not null,
    ScheduleEndDate datetime not null,
    BackgroundColor varchar(30) not null,
    UserID int not null,
    foreign key (UserID) references User(UserID) on delete cascade on update cascade
    );
    select date_format(ScheduleStartDate,'%Y-%M-%D %H:%i') as date from UserSchedule;
    select date_format(ScheduleEndDate,'%Y-%M-%D %H:%i') as date from UserSchedule;

---
+++ GroupwareL11/src/main/java/com/mju/groupware/service/CalenderService.java
@@ -0,0 +1,15 @@
+package com.mju.groupware.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.mju.groupware.dto.Calender;
+
+public interface CalenderService {
+
+	int InsertSchedule(Calender calender);
+
+	List<HashMap<String, Object>> SelectSchedule(int userId);
+
+	int SelectUserIdForCalender(String loginID);
+}
---
+++ GroupwareL11/src/main/java/com/mju/groupware/service/CalenderServiceImpl.java
@@ -0,0 +1,38 @@
+package com.mju.groupware.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mju.groupware.dao.CalenderDao;
+import com.mju.groupware.dto.Calender;
+
+@Service
+public class CalenderServiceImpl implements CalenderService {
+
+	@Autowired
+	private CalenderDao calenderDao;
+
+	@Override
+	public int InsertSchedule(Calender calender) {
+		int count = calenderDao.InsertSchedule(calender);
+		return count;
+	}
+
+	@Override
+	public List<HashMap<String, Object>> SelectSchedule(int userID) {
+		return calenderDao.SelectSchedule(userID);
+	}
+
+	@Override
+	public int SelectUserIdForCalender(String loginID) {
+		Integer userID = calenderDao.SelectUserIdForCalender(loginID);
+		if (userID == null) {
+			return 0;
+		} else {
+			return userID;
+		}
+	}
+}
