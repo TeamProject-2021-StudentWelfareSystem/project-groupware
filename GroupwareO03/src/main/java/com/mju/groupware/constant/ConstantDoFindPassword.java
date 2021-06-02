9ee6741baca245567ec57f8dca65abd0cbb0809b
lsb02 <subin2951@gmail.com>
2021-06-02 18:30:39 +0900

    후기 작성 화면 분리

---
+++ GroupwareO03/src/main/java/com/mju/groupware/constant/ConstantDoFindPassword.java
@@ -0,0 +1,32 @@
+package com.mju.groupware.constant;
+
+public class ConstantDoFindPassword {
+	private String UName;
+	private String Pwd;
+	private String AuthNum;
+
+	public String getAuthNum() {
+		return AuthNum;
+	}
+
+	public void setAuthNum(String authNum) {
+		AuthNum = authNum;
+	}
+
+	public String getPwd() {
+		return Pwd;
+	}
+
+	public void setPwd(String pwd) {
+		Pwd = pwd;
+	}
+
+	public String getUName() {
+		return UName;
+	}
+
+	public void setUName(String uName) {
+		UName = uName;
+	}
+
+}
