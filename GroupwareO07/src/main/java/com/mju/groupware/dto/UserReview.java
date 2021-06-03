385f486b142596e89e0edf0472377242e0abc431
Honam Kim <48897023+tigerpoint123@users.noreply.github.com>
2021-06-03 20:25:19 +0900

    서버 머지중1

---
+++ GroupwareO07/src/main/java/com/mju/groupware/dto/UserReview.java
@@ -0,0 +1,62 @@
+package com.mju.groupware.dto;
+
+public class UserReview {
+	private String Positive;
+	private String Contribute;
+	private String Respect;
+	private String Flexible;
+	private String ClassName;
+	private String ClassProfessorName;
+	private String ReviewDate;
+	private String UserID;
+	
+	public String getPositive() {
+		return Positive;
+	}
+	public void setPositive(String positive) {
+		Positive = positive;
+	}
+	public String getContribute() {
+		return Contribute;
+	}
+	public void setContribute(String contribute) {
+		Contribute = contribute;
+	}
+	public String getRespect() {
+		return Respect;
+	}
+	public void setRespect(String respect) {
+		Respect = respect;
+	}
+	public String getFlexible() {
+		return Flexible;
+	}
+	public void setFlexible(String flexible) {
+		Flexible = flexible;
+	}
+	public String getClassName() {
+		return ClassName;
+	}
+	public void setClassName(String className) {
+		ClassName = className;
+	}
+	public String getClassProfessorName() {
+		return ClassProfessorName;
+	}
+	public void setClassProfessorName(String classProfessorName) {
+		ClassProfessorName = classProfessorName;
+	}
+	public String getReviewDate() {
+		return ReviewDate;
+	}
+	public void setReviewDate(String reviewDate) {
+		ReviewDate = reviewDate;
+	}
+	public String getUserID() {
+		return UserID;
+	}
+	public void setUserID(String userID) {
+		UserID = userID;
+	}
+	
+}
