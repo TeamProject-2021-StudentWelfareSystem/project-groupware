b29ee0e6b41d847edb840b5e493acd3aaa9c9e84
YunjeongChoi <48897085+yjyjChoi@users.noreply.github.com>
2021-10-02 16:20:39 +0900

    교수 로직 제거

---
+++ GroupwareQ09/src/main/java/com/mju/groupware/constant/ConstantDoSignUp.java
@@ -0,0 +1,96 @@
+package com.mju.groupware.constant;
+
+public class ConstantDoSignUp {
+   private String Pwd;
+   private String PhoneNum;
+   private String SNum;
+   private String SName;
+   private String SRole;
+   private String PNum;
+   private String PName;
+   private String PRole;
+   private String SSUrl;
+   private String SLUrl;
+
+   
+   public String getSLUrl() {
+      return SLUrl;
+   }
+
+   public void setSLUrl(String sLUrl) {
+      SLUrl = sLUrl;
+   }
+
+   public String getSSUrl() {
+      return SSUrl;
+   }
+
+   public void setSSUrl(String sSUrl) {
+      SSUrl = sSUrl;
+   }
+
+   public String getSRole() {
+      return SRole;
+   }
+
+   public void setSRole(String sRole) {
+      SRole = sRole;
+   }
+
+   public String getSName() {
+      return SName;
+   }
+
+   public void setSName(String sName) {
+      SName = sName;
+   }
+
+   public String getSNum() {
+      return SNum;
+   }
+
+   public void setSNum(String sNum) {
+      SNum = sNum;
+   }
+
+   public String getPNum() {
+      return PNum;
+   }
+
+   public void setPNum(String pNum) {
+      PNum = pNum;
+   }
+
+   public String getPName() {
+      return PName;
+   }
+
+   public void setPName(String pName) {
+      PName = pName;
+   }
+
+   public String getPRole() {
+      return PRole;
+   }
+
+   public void setPRole(String pRole) {
+      PRole = pRole;
+   }
+
+   public String getPhoneNum() {
+      return PhoneNum;
+   }
+
+   public void setPhoneNum(String phoneNum) {
+      PhoneNum = phoneNum;
+   }
+
+   public String getPwd() {
+      return Pwd;
+   }
+
+   public void setPwd(String pwd) {
+      Pwd = pwd;
+   }
+
+}
