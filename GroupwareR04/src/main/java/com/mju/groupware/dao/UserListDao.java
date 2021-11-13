5ea56b9d8e8314b2facd61da38aa0447d030cc09
Honam Kim <48897023+tigerpoint123@users.noreply.github.com>
2021-11-13 17:45:49 +0900

    로그인 단위 테스트 성공 버전

---
+++ GroupwareR04/src/main/java/com/mju/groupware/dao/UserListDao.java
@@ -0,0 +1,28 @@
+package com.mju.groupware.dao;
+
+import java.util.List;
+
+import com.mju.groupware.dto.Criteria;
+import com.mju.groupware.dto.UserList;
+
+public interface UserListDao {
+
+	public List<UserList> SelectUserlist() throws Exception;
+
+	public List<UserList> SelectDormantUserList();
+
+	public List<UserList> SelectWithdrawalUserList();
+
+	public int CountTotalUserList(Criteria cri);
+
+	public List<UserList> SelectUserListPN(Criteria cri);
+	
+	public int CountTotalDormantUserList(Criteria cri);
+
+	public List<UserList> SelectDormantUserListPN(Criteria cri);
+
+	public int CountTotalWithdrawalUserList(Criteria cri);
+
+	public List<UserList> SelectWithdrawalUserListPN(Criteria cri);
+
+}
---
+++ GroupwareR04/src/main/java/com/mju/groupware/dto/Criteria.java
@@ -0,0 +1,96 @@
+package com.mju.groupware.dto;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class Criteria { // 게시글 조회 쿼리에 전달될 파라미터를 담게 될 클래스
+	private int page; // 현재 페이지 번호
+    private int perPageNum; // 한 페이지당 보여줄 게시글의 개수
+    private String UserLoginID;
+    private int UserID;
+    private String searchType;
+    private String keyword;
+    
+	public int getPageStart() {
+        return (this.page-1)*perPageNum;
+    }
+    
+    public Criteria() {
+        this.page = 1;
+        this.perPageNum = 10;
+        this.searchType = null;
+        this.keyword = null;
+    }
+    
+    public int getPage() {
+        return page;
+    }
+    public void setPage(int page) {
+        if(page <= 0) {
+            this.page = 1;
+        } else {
+            this.page = page;
+        }
+    }
+    public int getPerPageNum() {
+        return perPageNum;
+    }
+    public void setPerPageNum(int pageCount) {
+        int cnt = this.perPageNum;
+        if(pageCount != cnt) {
+            this.perPageNum = cnt;
+        } else {
+            this.perPageNum = pageCount;
+        }
+    }
+
+	public int getUserID() {
+		return UserID;
+	}
+
+	public void setUserID(int userID) {
+		UserID = userID;
+	}
+
+	public String getUserLoginID() {
+		return UserLoginID;
+	}
+
+	public void setUserLoginID(String userLoginID) {
+		UserLoginID = userLoginID;
+	}
+	
+	public String getSearchType() {
+		return searchType;
+	}
+
+	public void setSearchType(String searchType) {
+		this.searchType = searchType;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public String makeQuery() {
+		UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.newInstance()
+				.queryParam("page", this.page)
+				.queryParam("perPageNum", this.perPageNum);
+		
+		if(searchType != null) {
+			uriComponentsBuilder
+				.queryParam("searchType", this.searchType)
+				.queryParam("keyword", this.keyword);
+		}
+		return uriComponentsBuilder.build().encode().toString();
+	}
+	
+	@Override
+	public String toString() {
+		return "Criteria [page=" + page + ", perPageNum=" + perPageNum + "]";
+	}
+
+}
